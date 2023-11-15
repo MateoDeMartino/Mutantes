557d396c2b326c607e3cd0e85f8c729114340ad0
mdemartino <mateodemartino@gmail.com>
2023-11-15 09:40:53 -0300

    Mejorando interfaz

--- mutantes.py
+++ mutantes.py
@@ -1,10 +1,11 @@
 def main():
+    print("BUSCADOR DE MUTANTES")
     letras = ("A","T","C","G")
     
     dna = []
     while(len(dna)<6):
-        codigo = input("Ingrese el codigo: ")
-        print("validando caracteres")
+        codigo = input("Ingrese la cadena del ADN: ")
+        print("Validando caracteres")
         if len(codigo) == 6:
             contiene = all(letra.upper() in letras for letra in codigo)
             if contiene:
@@ -16,9 +17,8 @@ def main():
         else:
             print("el codigo no tiene 6 caracteres,ingreselo nuevamente")
             
-    # dna = ['atcgtc', 'agttat', 'tcatta', 'agactg', 'tatcct', 'ggacca']
-
-    result = is_mutant(dna)
+    print("Validando ADN")
+    result = mutante(dna)
 
     if result:
         print("Es un mutante.")
@@ -26,23 +26,26 @@ def main():
         print("No es un mutante.")
 
 
-def is_mutant(dna):
+def mutante(dna):
     n = len(dna)
     count = 0
     
-    # Verifica filas horizontales
+    # Verifica filas
+    print("Verificando filas")
     for i in range(n):
         for j in range(n - 3):
             if dna[i][j] == dna[i][j + 1] == dna[i][j + 2] == dna[i][j + 3]:
                 count += 1
 
-    # Verifica columnas verticales
+    # Verifica columnas
+    print("Verificando columnas")
     for i in range(n - 3):
         for j in range(n):
             if dna[i][j] == dna[i + 1][j] == dna[i + 2][j] == dna[i + 3][j]:
                 count += 1
 
     # Verifica diagonales
+    print("Verificando diagonales")
     for i in range(n - 3):
         for j in range(n - 3):
             if dna[i][j] == dna[i + 1][j + 1] == dna[i + 2][j + 2] == dna[i + 3][j + 3]:
